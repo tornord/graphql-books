69521d7793b7698b3bf6d11417077e7aa66ddbe5
Tor Nordqvist <tornord@gmail.com>
2024-05-26 20:29:12 +0200

    server-side-call

--- src/server.ts
+++ src/server.ts
@@ -4,15 +4,20 @@ import { makeExecutableSchema } from "@graphql-tools/schema";
 import { createYoga } from "graphql-yoga";
 import { createJsonDataLoaders, createPostgresLoaders, getResolvers } from "./resolvers";
 import { typeDefinitions } from "./schema";
+import { graphql, GraphQLArgs } from "graphql";
 
 const app = express();
 const PORT = 3000;
 
-const loaders = createPostgresLoaders();
+const loaders = createJsonDataLoaders(); // createPostgresLoaders();
 const executableSchema = makeExecutableSchema({
   resolvers: [getResolvers(loaders)],
   typeDefs: [typeDefinitions],
 });
+
+const x = await graphql({ schema: executableSchema, source: "{ bookById(id: \"1\") { id title author { name } } }" });
+console.log(JSON.stringify(x.data));
+console.log((x.data as any).bookById.author.name);
 
 const yoga = createYoga({ schema: executableSchema });
 app.use("/graphql", yoga);
